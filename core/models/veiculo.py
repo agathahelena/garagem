1890cd0a3a4d13d157570c89516a384c77a8d6be
agathahelena <agatha.helena.so@gmail.com>
2026-03-24 15:54:37 -0300

    Feat : Criação da api veiculo, e conclussão do trabalho

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -4,7 +4,9 @@ from django.db import models
 class Veiculo(models.Model):
     ano = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
-    categoria = models.CharField(max_length=80)
+    modelo = models.ForeignKey('Modelo', on_delete=models.CASCADE, null=True, blank=True)
+    cor = models.ForeignKey('Cor', on_delete=models.CASCADE, null=True, blank=True)
+    acessorio = models.ManyToManyField('Acessorio', blank=True)
 
     def __str__(self):
-        return str(self.id) + " " + self.nome
+        return str(self.id) + " " + self.modelo.nome + " " + self.cor.nome
